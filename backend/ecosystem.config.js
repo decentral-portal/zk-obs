db33373a3c83c839a3c63f6546bc411ce97bb0a6
abner <fantasrulai@gmail.com>
2023-01-04 21:34:02 +0800

    update

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -9,5 +9,15 @@ module.exports = {
     env_development: {
       NODE_ENV: 'development'
     }
+  }, {
+    name   : 'ts-api',
+    script: 'npm',
+    args : 'run start:api',
+    env_production: {
+      NODE_ENV: 'production'
+    },
+    env_development: {
+      NODE_ENV: 'development'
+    }
   }]
 };
