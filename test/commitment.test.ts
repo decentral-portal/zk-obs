c6d573f063efc9eb339f3c192b9dedaae87d45b5
abner <fantasrulai@gmail.com>
2023-01-05 14:32:37 +0800

    update

--- test/commitment.test.ts
+++ test/commitment.test.ts
@@ -1,21 +1,50 @@
-import { ethers } from 'ethers';
-import commitmentData from './example/zkobs-p1/0_register-acc1-p5-commitment.json';
-import calldata from './example/zkobs-p1/0_register-acc1-p5-8-8-4-8-calldata-raw.json';
+import fs from 'fs';
 import { expect } from 'chai';
 import { stateToCommitment } from './helper/helper';
+import { resolve } from 'path';
+const BaseDir = './test/example/zkobs-10-8-4';
 
+function initTestData(baseDir: string) {
+  const result = [];
+  const files = fs.readdirSync(baseDir, {
+    withFileTypes: true,
+  });
+  for (let index = 0; index < files.length; index++) {
+    const file = files[index];
+    // if (file.isDirectory()) {
+    //   const r: any = initTestData(resolve(baseDir, file.name));
+    //   result.push(r);
+    // }
+    if (file.isFile() && file.name.endsWith('-commitment.json')) {
+      const name = file.name.replace('-commitment.json', '');
+      const commitmentPath = resolve(baseDir, file.name);
+      const calldataPath = resolve(baseDir, `${name}-calldata-raw.json`);
+      const commitmentData = JSON.parse(
+        fs.readFileSync(commitmentPath, 'utf-8'),
+      );
+      const callData = JSON.parse(fs.readFileSync(calldataPath, 'utf-8'));
+      result.push({
+        path: resolve(baseDir, file.name),
+        commitmentData,
+        callData,
+      });
+    }
+  }
+  return result;
+}
 describe('compare commitment date sha256', function main() {
+  const result = initTestData(BaseDir);
   it('compare commitment date sha256', async function () {
-    const { commitment, commitmentMessage, commitmentHashOrigin } =
-      stateToCommitment(commitmentData);
-
-    console.log({
-      commitmentMessage,
-      commitmentHashOrigin,
-      commitment,
-      actualInput: calldata[3][0],
-    });
-
-    expect(commitment).to.be.eq(calldata[3][0]);
+    for (let index = 0; index < result.length; index++) {
+      const { path, commitmentData, callData } = result[index];
+      const { commitment, commitmentMessage, commitmentHashOrigin } =
+        stateToCommitment(commitmentData);
+      const isSame = commitment === callData[3][0];
+      console.log({
+        path,
+        isSame,
+      });
+      expect(commitment).to.be.eq(callData[3][0]);
+    }
   });
 });
