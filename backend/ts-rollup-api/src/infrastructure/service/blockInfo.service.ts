ee85a69ead97c7b024a9d82a61e37aee94ea1693
gsonLiang <yuanyu.liang@tkspring.com>
2023-01-06 15:17:22 +0800

    change queryBuilder name

--- backend/ts-rollup-api/src/infrastructure/service/blockInfo.service.ts
+++ backend/ts-rollup-api/src/infrastructure/service/blockInfo.service.ts
@@ -18,10 +18,10 @@ export class BlockInformationServcie {
   async getTransactionInfoByBlockNumber(blockNumber: number, paginationDto: PaginationDto): Promise<TransactionInfoPagination> {
     const { pageNumber, perPage } = paginationDto;
     console.log({ blockNumber, pageNumber, perPage });
-    const queryBuilder = this.transactionInfoRepository.createQueryBuilder('TransactionInfo');
-    queryBuilder.from(TransactionInfo, 'TransactionInfo');
-    queryBuilder.where('TransactionInfo."blockNumber" = :blockNumber', { blockNumber });
-    queryBuilder.orderBy('TransactionInfo."txId"', 'DESC');
+    const queryBuilder = this.transactionInfoRepository.createQueryBuilder('transactionInfo');
+    // queryBuilder.from(TransactionInfo, 'TransactionInfo');
+    queryBuilder.where('transactionInfo."blockNumber" = :blockNumber', { blockNumber });
+    queryBuilder.orderBy('transactionInfo."txId"', 'DESC');
     queryBuilder.skip((pageNumber - 1) * perPage);
     queryBuilder.take(perPage);
     console.log(queryBuilder.getSql());
@@ -56,7 +56,6 @@ export class BlockInformationServcie {
   async getBlockInformations(paginationDto: PaginationDto): Promise<BlockInformationPagination> {
     const { pageNumber, perPage } = paginationDto;
     const queryBuilder = this.blockInformationRepository.createQueryBuilder('blockInformation');
-    // queryBuilder.from(BlockInformation, 'blockInformation');
     queryBuilder.orderBy('blockInformation."blockNumber"', 'DESC');
     queryBuilder.skip((pageNumber - 1) * perPage);
     queryBuilder.take(perPage);
@@ -81,8 +80,8 @@ export class BlockInformationServcie {
     };
   }
   async getLatestBlockInformation(): Promise<BlockInformationWithTxDto> {
-    const queryBuilder = this.blockInformationRepository.createQueryBuilder('BlockInformation');
-    queryBuilder.orderBy('BlockInformation."blockNumber"', 'DESC');
+    const queryBuilder = this.blockInformationRepository.createQueryBuilder('blockInformation');
+    queryBuilder.orderBy('blockInformation."blockNumber"', 'DESC');
     queryBuilder.limit(1);
     const [block] = await queryBuilder.getMany();
     const transactions = await this.getTransactionInfoByBlockNumber(block.blockNumber, { pageNumber: 1, perPage: 10 });
@@ -101,9 +100,9 @@ export class BlockInformationServcie {
     };
   }
   async getBlockInformationByBlockNumber(blockNumber: number): Promise<BlockInformationWithTxDto> {
-    const queryBuilder = this.blockInformationRepository.createQueryBuilder('BlockInformation');
+    const queryBuilder = this.blockInformationRepository.createQueryBuilder('blockInformation');
     // queryBuilder.from(BlockInformation, 'blockInformation');
-    queryBuilder.where('BlockInformation."blockNumber" = :blockNumber', { blockNumber });
+    queryBuilder.where('blockInformation."blockNumber" = :blockNumber', { blockNumber });
     const [block] = await queryBuilder.getMany();
     const transactions = await this.getTransactionInfoByBlockNumber(block.blockNumber, { pageNumber: 1, perPage: 10 });
     return {
